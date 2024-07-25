d122f00d204298cdc952a0144979d39f6058fd9d
mugihouse <mugi.stnk100122@gmail.com>
2024-07-26 02:43:32 +0900

    Add: propsへ値を引き渡し

--- 04_react_basic/src/090_props/start/Example.jsx
+++ 04_react_basic/src/090_props/start/Example.jsx
@@ -1,5 +1,18 @@
 import Child from "./components/Child";
 
-const Example = () => <Child />;
+const Example = () => {
+  const hello = (arg) => `Hello ${arg}`;
+  return (
+    <>
+      <Child
+        color="red"
+        num={123}
+        fn={hello}
+        bool
+        obj={{ name: "Tom", age: 18 }}
+      />
+    </>
+  );
+};
 
 export default Example;
