eab1478df94d1aae60dd2ea1a8f1248899b6da72
mugihouse <mugi.stnk100122@gmail.com>
2024-07-26 15:01:53 +0900

    Add: useStateを使用して入力した値を表示

--- 05_state_and_event/src/020_useState/start/Example.jsx
+++ 05_state_and_event/src/020_useState/start/Example.jsx
@@ -1,8 +1,10 @@
-const Example = () => {
+import { useState } from "react";
 
+const Example = () => {
+  let [val, setVal] = useState();
   return (
     <>
-      
+      <input type="text" onChange={(e) => setVal(e.target.value)} /> = {val}
     </>
   );
 };
