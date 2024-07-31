19f7e53ec9dc3c8e338d4e01c81160cc256a2c13
mugihouse <mugi.stnk100122@gmail.com>
2024-07-31 18:14:35 +0900

    Add: ラジオボックス

--- 06_control_and_form/src/090_radio/start/Example.jsx
+++ 06_control_and_form/src/090_radio/start/Example.jsx
@@ -1,10 +1,29 @@
+import { useState } from "react";
+
 const Example = () => {
+  const [fruit, setFruit] = useState("");
+
+  const RADIO_COLLECTION = ["Apple", "Banana", "Cherry"];
+  const onChange = (e) => {
+    setFruit(e.target.value);
+  };
   return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
+    <div>
+      {RADIO_COLLECTION.map((value) => {
+        return (
+          <label key={value}>
+            <input
+              type="radio"
+              value={value}
+              checked={fruit === value}
+              onChange={onChange}
+            />
+            {value}
+          </label>
+        );
+      })}
+      <h3>私は{fruit}が食べたい</h3>
+    </div>
   );
 };
 
