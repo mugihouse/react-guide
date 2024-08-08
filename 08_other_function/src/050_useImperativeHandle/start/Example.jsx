dd13d02389457b6b7517188edf798320fb2b1dd0
mugihouse <mugi.stnk100122@gmail.com>
2024-08-09 00:08:55 +0900

    Add: useImperativeHandleでrefの制御

--- 08_other_function/src/050_useImperativeHandle/start/Example.jsx
+++ 08_other_function/src/050_useImperativeHandle/start/Example.jsx
@@ -1,3 +1,4 @@
+import { useImperativeHandle } from "react";
 import { useRef, forwardRef } from "react";
 
 /* POINT forwardRef
@@ -6,7 +7,15 @@ refは、親から子コンポーネントへprops形式で渡して参照する
 参照したい場合は子コンポーネント内でfowardRefを使用する必要があります。
 */
 const Input = forwardRef((props, ref) => {
-  return <input type="text" ref={ref} />;
+  const inputRef = useRef();
+
+  useImperativeHandle(ref, () => ({
+    myFocus() {
+      inputRef.current.focus();
+    },
+  }));
+
+  return <input type="text" ref={inputRef} />;
 });
 
 const Example = () => {
@@ -14,7 +23,7 @@ const Example = () => {
   return (
     <>
       <Input ref={ref} />
-      <button onClick={() => ref.current.focus()}>
+      <button onClick={() => ref.current.myFocus()}>
         インプット要素をフォーカスする
       </button>
     </>
