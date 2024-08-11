af097620d29a9f00e22f1218dd95050d2e09b4f4
mugihouse <mugi.stnk100122@gmail.com>
2024-08-11 22:21:38 +0900

    Add: ヘッダーコンポーネント作成

--- 11_hooks_p1/src/050_context_file/start/components/Header.jsx
+++ 11_hooks_p1/src/050_context_file/start/components/Header.jsx
@@ -1,5 +1,30 @@
+import { useTheme, useUpdateTheme } from "../context/themeContext";
+
 const Header = () => {
-  
+  const THEMES = ["light", "dark", "red"];
+
+  const theme = useTheme();
+  const setTheme = useUpdateTheme();
+
+  const changeTheme = (e) => setTheme(e.target.value);
+
+  return (
+    <header className={`content-${theme}`}>
+      {THEMES.map((_theme) => {
+        return (
+          <label key={_theme}>
+            <input
+              type="radio"
+              value={_theme}
+              checked={theme === _theme}
+              onChange={changeTheme}
+            />
+            {_theme}
+          </label>
+        );
+      })}
+    </header>
+  );
 };
 
 export default Header;
