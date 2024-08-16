e7e565088e3def46233323316286a7691b1ec214
mugihouse <mugi.stnk100122@gmail.com>
2024-08-16 13:38:11 +0900

    Add: useEffctが実行されるタイミングを確認

--- 12_hooks_p2/src/030_useEffect_cleanup/start/Example.jsx
+++ 12_hooks_p2/src/030_useEffect_cleanup/start/Example.jsx
@@ -1,18 +1,31 @@
 import { useEffect, useState } from "react";
 
 const Example = () => {
+  const [isDisp, setIsDisp] = useState(true);
+
+  return (
+    <>
+      {isDisp && <Timer />}
+      <button onClick={() => setIsDisp((prev) => !prev)}>トグル</button>
+    </>
+  );
+};
+const Timer = () => {
   const [time, setTime] = useState(0);
 
   useEffect(() => {
-    console.log('useEffect is called');
+    console.log("init");
     window.setInterval(() => {
-      setTime(prev => prev + 1);
+      setTime((prev) => prev + 1);
     }, 1000);
-  }, [])
-  
+    return () => console.log("end");
+  }, []);
+
   useEffect(() => {
-    document.title = 'counter:' + time;
-    window.localStorage.setItem('time-key-end', time)
+    console.log("init time");
+    document.title = "counter:" + time;
+    window.localStorage.setItem("time-key-end", time);
+    return () => console.log("end time");
   }, [time]);
 
   return (
@@ -20,7 +33,7 @@ const Example = () => {
       <time>{time}</time>
       <span>秒経過</span>
     </h3>
-    );
+  );
 };
 
 export default Example;
