1ec7994aed9a43a6559fa43822ace268cf01e091
mugihouse <mugi.stnk100122@gmail.com>
2024-08-24 13:39:26 +0900

    Add: ミドルウェアをインポートしreduxへ導入

--- 13_redux/start/src/070_middleware/store/index.js
+++ 13_redux/start/src/070_middleware/store/index.js
@@ -1,8 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./modules/counter";
+import logger from "./middleware/logger";
 
 export default configureStore({
   reducer: {
-    counter: reducer
-  }
+    counter: reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    const middlewares = getDefaultMiddleware();
+    const newMiddlewares = middlewares.concat(logger);
+    return newMiddlewares;
+  },
 });
