fa7d0bf08aa107f4981ad8296fe4c8c9d3c8c2da
mugihouse <mugi.stnk100122@gmail.com>
2024-08-25 17:45:29 +0900

    Add: axiosでjsonを受け取る

--- 16_rest_api/src/040_axios_get_request/start/Example.jsx
+++ 16_rest_api/src/040_axios_get_request/start/Example.jsx
@@ -1,4 +1,25 @@
+import { useEffect } from "react";
+import axios from "axios";
+
 const Example = () => {
+  useEffect(() => {
+    // getメソッドで返ってくる値はPromiseオブジェクト
+    // thenで繋ぐかawaitを記述
+    axios.get("http://localhost:3003/user").then((res) => {
+      console.log(res.data);
+    });
+
+    // const res = axios.get("http://localhost:3003/user");
+    // console.log(res)と記述するとresを受け取る前にconsole.logが実行されてしまう
+  }, []);
+
+  // useEffect(() => {
+  //   const getUser = async () => {
+  //     const res = await axios.get("http://localhost:3003/user");
+  //     console.log(res.data);
+  //   };
+  //   getUser();
+  // });
   return <></>;
 };
 
