aa20e9dc4ebdd54b564bb0089abde2e40d3714ce
mugihouse <mugi.stnk100122@gmail.com>
2024-08-26 11:41:06 +0900

    Add: buildの設定

--- 21_nextjs_app_router/start/next.config.mjs
+++ 21_nextjs_app_router/start/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  trailingSlash: true, // URLの最後に/をつけるか
+};
 
 export default nextConfig;
