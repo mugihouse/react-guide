2f1054a2bc3d08bb6e14f7eec82ec0ebcd1f23bf
mugihouse <mugi.stnk100122@gmail.com>
2024-08-28 10:05:38 +0900

    Add: 動的メタデータの作成

--- 21_nextjs_app_router/start/src/app/030_SG_fetch/[id]/page.js
+++ 21_nextjs_app_router/start/src/app/030_SG_fetch/[id]/page.js
@@ -1,6 +1,16 @@
 import { ENDPOINT } from "@/constants";
 import Article from "../../../components/article";
 
+export async function generateMetadata({ params }) {
+  const article = await fetch(`${ENDPOINT}/${params.id}`).then((res) =>
+    res.json()
+  );
+  return {
+    title: article.title,
+    description: article.text,
+  };
+}
+
 export async function generateStaticParams() {
   const data = await fetch(ENDPOINT).then((res) => res.json());
   return data.map((record) => ({
